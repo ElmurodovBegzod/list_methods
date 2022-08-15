d003bdaa99dceb4893c7423283840f553ffda236
Begzod <ebegzod477@gmail.com>
2022-08-16 00:31:33 +0500

    code04

--- bin/list_method04.dart
+++ bin/list_method04.dart
@@ -8,4 +8,18 @@
         list: return answer
 */
 
-void main() {}
+int func(List list1, int i) {
+  list1[i];
+  List x = [];
+  x.add(list1[i]);
+  return list1[i];
+}
+
+void main() {
+  print(func([
+    3,
+    2,
+    8,
+    1,
+  ], 3));
+}
